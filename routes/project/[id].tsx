841aa7a3427cd4a30ffc72f6a30964230fec1629
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-02-19 20:37:44 -0500

    add render side markdown to boy

--- routes/project/[id].tsx
+++ routes/project/[id].tsx
@@ -1,6 +1,7 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { loadProject } from "../../utils/projects.ts";
 import { TProject } from "../../types.d.ts";
+import { CSS } from "$gfm/mod.ts";
 
 export const handler: Handlers = {
   async GET(request: Request, context) {
@@ -13,15 +14,20 @@ export const handler: Handlers = {
 
 export default function PagePost(props: PageProps) {
   const project: TProject = props?.data.project;
-  console.log({ props });
+
   return (
     <article class="p-4">
       <h1 class="text-2xl font-bold">
         {project.title}
       </h1>
-      <p class="text-lg">
-        {project.body}
-      </p>
+      {/* refer to Colombia  */}
+      <time>{Intl.DateTimeFormat("co").format(project.date)}</time>
+      <style dangerouslySetInnerHTML={{ __html: CSS }}></style>
+      <div
+        class="markdown-body"
+        dangerouslySetInnerHTML={{ __html: project.body }}
+      >
+      </div>
     </article>
   );
 }
--- utils/projects.ts
+++ utils/projects.ts
@@ -1,14 +1,17 @@
 import { TProject } from "../types.d.ts";
 import { extract } from "$std/encoding/front_matter/any.ts";
+import { render } from "$gfm/mod.ts";
 
 export async function loadProject(id: string): Promise<TProject | null> {
-  let raw: string;
-  try {
-    raw = await Deno.readTextFile(`./content/projects/${id}.md`);
-  } catch {
+  const raw: string | null = await Deno
+    .readTextFile(`./content/projects/${id}.md`)
+    .catch(() => null);
+
+  if (!raw) {
     return null;
   }
-  const { attrs, body, frontMatter } = extract(raw);
+
+  const { attrs, body } = extract(raw);
 
   // declare de <string, unknown> value to manipulare
   const params = attrs as Record<string, string>;
@@ -18,7 +21,7 @@ export async function loadProject(id: string): Promise<TProject | null> {
     date: new Date(params.date),
     excerpt: params.excerpt,
     title: params.title,
-    body,
+    body: render(body),
   };
 
   return project;

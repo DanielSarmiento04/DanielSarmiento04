e21294e7226e227e489793635bf192cdd084fb74
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-03-02 22:57:56 -0500

    improve ui ux

--- components/CBlogs.tsx
+++ components/CBlogs.tsx
@@ -12,39 +12,46 @@ interface CBlogsProps {
 export function CBlogs(props: CBlogsProps) {
   const { blogs } = props;
   return (
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {blogs.map((blog: TBlog) => (
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white mb-4 ml-4">
-          <a
-            href={`blog/${blog.id}`}
-            target="_black"
-            rel="noopener noreferrer"
+    <div class="flex flex-col justify-center items-center">
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {blogs.map((blog: TBlog, index: number) => (
+          <div
+            class={`max-w-sm rounded overflow-hidden shadow-lg bg-white mb-4 ml-4 ${
+              index === blogs.length - 1 ? "justify-self-end" : ""
+            }`}
+            key={blog.id}
           >
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">{blog.title}</div>
-              <p class="text-gray-600 text-sm mt-2">
-                {Intl.DateTimeFormat("es", {
-                  timeZone: "GMT",
-                }).format(blog.date)}
-              </p>
-              <p class="text-gray-700 text-base">
-                {blog.description}
-              </p>
-            </div>
-            <div class="px-4 py-4 flex items-center justify-between">
-              <div class="flex ">
-                {blog.tags.map((tag) => (
-                  <span class="inline-block bg-gray-200 rounded-full px-6 py-1 text-sm font-semibold text-gray-700 ">
-                    {tag}
-                  </span>
-                ))}
+            <a
+              href={`blog/${blog.id}`}
+              target="_black"
+              rel="noopener noreferrer"
+            >
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">{blog.title}</div>
+                <p class="text-gray-600 text-sm mt-2">
+                  {Intl.DateTimeFormat("es", {
+                    timeZone: "GMT",
+                  }).format(blog.date)}
+                </p>
+                <p class="text-gray-700 text-base">
+                  {blog.description}
+                </p>
               </div>
-              <LikeButton />
-            </div>
-          </a>
-          {/* <img class="w-full" src="https://source.unsplash.com/random/400x200" alt="Random image"> */}
-        </div>
-      ))}
+              <div class="px-4 py-4 flex items-center justify-between">
+                <div class="flex ">
+                  {blog.tags.map((tag) => (
+                    <span class="inline-block bg-gray-200 rounded-full px-6 py-1 text-sm font-semibold text-gray-700 ">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <LikeButton />
+              </div>
+            </a>
+            {/* <img class="w-full" src="https://source.unsplash.com/random/400x200" alt="Random image"> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
